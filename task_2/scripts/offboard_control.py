7c58a7a11bccf1effbb0e6d36cb0b8379cb0ba14
ManasSashank <71089314+ManasSashank@users.noreply.github.com>
2021-12-03 13:06:51 +0530

    Update offboard_control.py

--- task_2/scripts/offboard_control.py
+++ task_2/scripts/offboard_control.py
@@ -18,10 +18,12 @@ This node publishes and subsribes the following topics:
     
 '''
 
+from re import X
 import rospy
 from geometry_msgs.msg import *
 from mavros_msgs.msg import *
 from mavros_msgs.srv import *
+import numpy as np
 
 
 class offboard_control:
@@ -30,9 +32,18 @@ class offboard_control:
     def __init__(self):
         # Initialise rosnode
         rospy.init_node('offboard_control', anonymous=True)
+        # Waiting untill the service starts
+
+    def paramset():
         rospy.wait_for_service('/mavros/param/set')
-        change_mode = rospy.ServiceProxy('/mavros/param/set', mavros_msgs.srv.SetMode)
-        change_mode("COM_RCL_EXCEPT", 2)
+        try:
+            change_mode = rospy.ServiceProxy('/mavros/param/set', mavros_msgs.srv.SetMode)
+            change_mode("COM_RCL_EXCEPT", 2)
+        except rospy.ServiceException as e:
+            print("Service param failed %s"%e)
+    
+
+        
     def setArm(self):
         # Calling to /mavros/cmd/arming to arm the drone and print fail message on failure
         rospy.wait_for_service('mavros/cmd/arming')  # Waiting untill the service starts 
@@ -52,8 +63,7 @@ class offboard_control:
         rospy.wait_for_service('mavros/set_mode')
         try:
 
-            set_ModeService = rospy.ServiceProxy(
-                'mavros/set_mode', mavros_msgs.srv.SetMode)
+            set_ModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.SetMode)
             set_ModeService(custom_mode="OFFBOARD")
 
         except rospy.ServiceException as e:
@@ -64,15 +74,16 @@ class stateMoniter:
     def __init__(self):
         self.state = State()
         # Instantiate a setpoints message
-
-        
+        self.pos = PositionTarget()
+        self.local_pos= Point(0,0,0)
+           
     def stateCb(self, msg):
         # Callback function for topic /mavros/state
         self.state = msg
     def posCb(self, msg):
-        self.local_pos.x = msg.pose.point.x
-        self.local_pos.y = msg.pose.point.y
-        self.local_pos.z = msg.pose.point.z
+        self.local_pos.x = msg.pose.position.x
+        self.local_pos.y = msg.pose.position.y
+        self.local_pos.z = msg.pose.position.z
 
     # Create more callback functions for other subscribers    
 
@@ -90,7 +101,7 @@ def main():
     rate = rospy.Rate(20.0)
 
     # Make the list of setpoints 
-    setpoints = [[0,0,10],[10,0,10],[10,10,10],[0,10,10],[0,0,10],[0,0,0]] #List to setpoints
+    setpoints = [(0,0,10),(10,0,10),(10,10,10),(0,10,10),(0,0,10),(0,0,0)] #List to setpoints
 
     # Similarly initialize other publishers 
 
@@ -107,6 +118,7 @@ def main():
     vel.linear.z = 0
     
     # Similarly add other containers 
+
 
     # Initialize subscriber 
     rospy.Subscriber("/mavros/state",State, stateMt.stateCb)
@@ -132,9 +144,25 @@ def main():
         ofb_ctl.offboard_set_mode()
         rate.sleep()
     print ("OFFBOARD mode activated")
+    i=0
+    pos.pose.position.x= setpoints[i][0]
+    pos.pose.position.y= setpoints[i][1]
+    pos.pose.position.z= setpoints[i][2]
+    def check_position():
+        desired = np.array((setpoints[i][0],setpoints[i][1],setpoints[i][2]))
+        pos = np.array((local_pos.x,
+                        local_pos.y,
+                        local_pos.z))
+        return np.linalg.norm(desired - pos) < 0.5
 
     # Publish the setpoints 
+    
+
+
     while not rospy.is_shutdown():
+        
+        
+
         '''
         Step 1: Set the setpoint 
         Step 2: Then wait till the drone reaches the setpoint, 
@@ -142,11 +170,29 @@ def main():
         Step 4: Once the drone reaches the setpoint, publish the next setpoint , repeat the process until all the setpoints are done  
 
 
-        Write your algorithm here 
+        Write your algorithm here
+         
         '''
+
+        stateMt
+        ofb_ctl
+        ofb_ctl.paramset
+        reached= check_position
+        if reached==True & i<=len(setpoints-1):
+            i+=1
+            
+
 
         local_pos_pub.publish(pos)
         local_vel_pub.publish(vel)
+         
+
+
+
+
+        
+
+        
         rate.sleep()
 
 if __name__ == '__main__':
