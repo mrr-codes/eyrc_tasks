6ad0abe9548ed5fc1f82bc4b8c26d7d7d277d341
ManasSashank <71089314+ManasSashank@users.noreply.github.com>
2021-12-12 17:38:52 +0530

    Update task_3lol.py

--- task_3/scripts/task_3lol.py
+++ task_3/scripts/task_3lol.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
+
+
+'''
+This is a boiler plate script that contains hint about different services that are to be used
+to complete the task.
+Use this code snippet in your code or you can also continue adding your code in the same file
+
+
+This python file runs a ROS-node of name offboard_control which controls the drone in offboard mode. 
+See the documentation for offboard mode in px4 here() to understand more about offboard mode 
+This node publishes and subsribes the following topics:
+
+	 Services to be called                   Publications                                          Subscriptions				
+	/mavros/cmd/arming                       /mavros/setpoint_position/local                       /mavros/state
+    /mavros/set_mode                         /mavros/setpoint_velocity/cmd_vel                     /mavros/local_position/pose   
+         
+    
+'''
+
+from re import X
 import rospy
 from geometry_msgs.msg import *
 from mavros_msgs.msg import *
 from mavros_msgs.srv import *
 import numpy as np
-from std_msgs.msg import String
+from std_msgs.msg import *
 
 
 class offboard_control:
 
     def __init__(self):
-    
+        # Initialise rosnode
         rospy.init_node('offboard_control', anonymous=True)
+        # Waiting untill the service starts
 
-
-    
-    
     def paramset():
         rospy.wait_for_service('/mavros/param/set')
         try:
@@ -25,10 +43,6 @@ class offboard_control:
         except rospy.ServiceException as e:
             print("Service param failed %s" % e)
 
-    
-    
-    
-    
     def setArm(self):
         # Calling to /mavros/cmd/arming to arm the drone and print fail message on failure
         # Waiting untill the service starts
@@ -43,7 +57,6 @@ class offboard_control:
 
         # Similarly delacre other service proxies
 
-
     def offboard_set_mode(self):
 
         # Call /mavros/set_mode to set the mode the drone to OFFBOARD
@@ -57,22 +70,22 @@ class offboard_control:
 
         except rospy.ServiceException as e:
             print("Service setting mode call failed: %s" % e)
-    
-    
     def setAutoLandMode(self):
         rospy.wait_for_service('mavros/set_mode')
-        try:
-            flightModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.SetMode)
-            flightModeService(custom_mode='AUTO.LAND')
-        except rospy.ServiceException as e:
-            print ("service set_mode call failed: %s. Autoland Mode could not be set" % e)
+        
+        set_ModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.SetMode)
+        set_ModeService(custom_mode='AUTO.LAND')
+        print("inside autoland")
+        #except rospy.ServiceException as e:
+            #print ("service set_mode call failed: %s. Autoland Mode could not be set" % e)
     
-
-
+    def gripper_activate(self,grip_control):
+        rospy.wait_for_service('activate_gripper')
+        gripper= rospy.ServiceProxy('activate_gripper',std_msgs.srv.Bool)
+        gripper(grip_control)
 
 
 class stateMoniter:
-
     def __init__(self):
         self.state = State()
         # Instantiate a setpoints message
@@ -88,20 +101,11 @@ class stateMoniter:
         self.local_pos.y = msg.pose.position.y
         self.local_pos.z = msg.pose.position.z
 
-        #callback to check if gripper can be activated or not
-    
-    
+    # Create more callback functions for other subscribers
     def gripper_check_clbk(self, msg):
         check_gripper = msg.data 
         #rospy.loginfo(check_gripper)
         return check_gripper
-
-
-
-
-
-
-
 
 
 def main():
@@ -109,22 +113,18 @@ def main():
     stateMt = stateMoniter()
     ofb_ctl = offboard_control()
 
-    
-    
     # Initialize publishers
-    local_pos_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
-    local_vel_pub = rospy.Publisher('mavros/setpoint_velocity/cmd_vel', Twist, queue_size=10)
-    
-    
-    
+    local_pos_pub = rospy.Publisher(
+        'mavros/setpoint_position/local', PoseStamped, queue_size=10)
+    local_vel_pub = rospy.Publisher(
+        'mavros/setpoint_velocity/cmd_vel', Twist, queue_size=10)
     # Specify the rate
     rate = rospy.Rate(20.0)
 
-
-
     # Make the list of setpoints
-    setpoints = [(0, 0, 3), (3, 0, 3), '''(3, 0, 0)''', (3, 0, 3), (3, 3, 3), '''(3, 3, 0)''', (3, 3, 3), (0, 0, 3), '''(0, 0, 0)''']  # List to setpoints
+    setpoints = [(0, 0, 3), (3, 0, 3),(3,0,3), (3,3,3), (3,3,3),(0,0,3)]  # List to setpoints
 
+    # Similarly initialize other publishers
 
     # Create empty message containers
     pos = PoseStamped()
@@ -140,38 +140,37 @@ def main():
 
     # Similarly add other containers
 
-
-
-
-
     # Initialize subscriber
-
     rospy.Subscriber("/mavros/state", State, stateMt.stateCb)
+
     rospy.Subscriber("/mavros/local_position/pose", PoseStamped, stateMt.posCb)
-    rospy.Subscriber('/gripper_check', String,stateMt.gripper_check_clbk)
-
-
+    rospy.Subscriber('/gripper_check', String ,stateMt.gripper_check_clbk)
 
     '''
     NOTE: To set the mode as OFFBOARD in px4, it needs atleast 100 setpoints at rate > 10 hz, so before changing the mode to OFFBOARD, send some dummy setpoints  
     '''
-    for i in range(100):
-        print('Sending dummy points')
-        local_pos_pub.publish(pos)
-        rate.sleep()
-
-
-    while not stateMt.state.armed:  #safety check later
+    def dummy_points():
+        for i in range(100):
+            print('Sending dummy points')
+            local_pos_pub.publish(pos)
+            rate.sleep()
+    dummy_points()
+    # Arming the drone
+    while not stateMt.state.armed:
         ofb_ctl.setArm()
         rate.sleep()
-        print("Armed!!")
+    print("Armed!!")
 
-    
+    # Switching the state to auto mode
     while not stateMt.state.mode == "OFFBOARD":
         ofb_ctl.offboard_set_mode()
         rate.sleep()
-        print("OFFBOARD mode activated")
+    print("OFFBOARD mode activated")
     i = 0
+    # pos.pose.position.x = setpoints[i][0]
+    # pos.pose.position.y = setpoints[i][1]
+    # pos.pose.position.z = setpoints[i][2]
+
     def check_position():
         desired = np.array((setpoints[i][0], setpoints[i][1], setpoints[i][2]))
         pos = np.array((stateMt.local_pos.x,
@@ -181,24 +180,49 @@ def main():
         
         return np.linalg.norm(desired - pos) < 0.5
 
-
-
-
+    # Publish the setpoints
 
     while not rospy.is_shutdown():
 
+        '''
+        Step 1: Set the setpoint 
+        Step 2: Then wait till the drone reaches the setpoint, 
+        Step 3: Check if the drone has reached the setpoint by checking the topic /mavros/local_position/pose 
+        Step 4: Once the drone reaches the setpoint, publish the next setpoint , repeat the process until all the setpoints are done  
+
+
+        Write your algorithm here
+
+        '''
 
         stateMt
-        ofb_ctl.setArm
-        ofb_ctl.offboard_set_mode
+        ofb_ctl.setArm()
+        
         reached = check_position()
-        if (reached == True) and (i <= 4):
-            i += 1
+        if reached==True and (i==1 or i==4 or i==5):
+
+
+            ofb_ctl.setAutoLandMode() 
+            print('Attempted to land')
+            #print(reached)
+            if (i<=5):
+                i = i + 1
+                if i==4:
+                    stateMt.gripper_check_clbk()
+                    ofb_ctl.gripper_activate(True)
+                elif i==5:
+                    stateMt.gripper_check_clbk()
+                    ofb_ctl.gripper_activate(False)
+
+
+        elif reached==True:
+            print("off", i)
+            i=i+1
+
             print(i)
-            if i==5:
-               offboard_control.setAutoLandMode
-            
-            
+            dummy_points()
+            ofb_ctl.offboard_set_mode()
+        print(setpoints[i])    
             
 
         pos.pose.position.x = setpoints[i][0]
