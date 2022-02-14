a4e6cd44cad15cd461a4b8d4519a33dfdd2247e9
ManasRR <manasramidi@gmail.com>
2022-02-14 14:19:35 +0530

    autoland update

--- task_5/scripts/autoland_truck.py
+++ task_5/scripts/autoland_truck.py
@@ -418,7 +418,7 @@ def drone_0():
             vi = 0.07                             #it's me
             previous_y_error = 0
             setpoints_0.extend([(stateMt.local_pos_0.x,stateMt.local_pos_0.y,6),stateMt.row_spawn_sp0[k],(0,0,4)])
-            stateMt.row_spawn_sp0.pop()
+            pop_ele = stateMt.row_spawn_sp0.pop()
             print('d0 Setpoints list as of now', setpoints_0)
 
         if  i==2 and flag_flip_pos_vol == True :
@@ -439,7 +439,7 @@ def drone_0():
                         vi = 0.1
                     print('d0 publishing set pt to decrease height to 1m')
                     pos_0.pose.position.x = stateMt.local_pos_0.x
-                    pos_0.pose.position.y = stateMt.local_pos_0.y
+                    pos_0.pose.position.y = pop_ele[1]
                     pos_0.pose.position.z = 1.5
                     local_pos_pub_0.publish(pos_0)
                     rospy.sleep(5)
@@ -537,11 +537,11 @@ def drone_0():
                 print("d0 Reached goal")
                 while x == 0:
                     setpoints_0.extend([stateMt.row_spawn_sp0[k],(0,4,4)])
-                    stateMt.row_spawn_sp0.pop()
+                    pop_ele =stateMt.row_spawn_sp0.pop()
                     
                     print('d0 after reaching goal setpoints are', setpoints_0)
                     x = x+1
-
+                    rospy.sleep(10)
                 i = i+1
                 print('d0 i increased to ', i, 'after reaching goal')
 
@@ -654,7 +654,7 @@ def drone_1():
             #k += 1
             previous_y_error = 0
             setpoints_1.extend([(stateMt.local_pos_1.x,stateMt.local_pos_1.y,7),stateMt.row_spawn_sp1[k],(0,0,4)])
-            stateMt.row_spawn_sp1.pop()
+            pop_ele = stateMt.row_spawn_sp1.pop()
             print('d1 Setpoints list as of now', setpoints_1)
 
         if  i==2 and flag_flip_pos_vol == True :
@@ -675,7 +675,7 @@ def drone_1():
                         vi = 0.1
                     print('d1 publishing set pt to decrease height to 1m')
                     pos_1.pose.position.x = stateMt.local_pos_1.x
-                    pos_1.pose.position.y = stateMt.local_pos_1.y
+                    pos_1.pose.position.y = pop_ele[1]
                     pos_1.pose.position.z = 1.5
                     local_pos_pub_1.publish(pos_1)
                     rospy.sleep(5)
@@ -775,10 +775,10 @@ def drone_1():
                     print(stateMt.row_spawn_sp1)
                     print(stateMt.row_spawn_sp1[k])
                     setpoints_1.extend([stateMt.row_spawn_sp1[k],(0,4,4)])
-                    stateMt.row_spawn_sp1.pop()
+                    pop_ele = stateMt.row_spawn_sp1.pop()
                     print('after reaching goal setpoints are', setpoints_1)
                     x = x+1
-
+                    rospy.sleep(10)
                 i = i+1
                 print('d1 i increased to ', i, 'after reaching goal')
 
